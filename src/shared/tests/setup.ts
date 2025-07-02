339e8d153eb6e901917d7386016a1a9c57dd31e3
Mohammad Toosi <m_dd@rogers.com>
2025-07-02 21:57:49 +0200

    fix: vitest error about canvas

--- src/shared/tests/setup.ts
+++ src/shared/tests/setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom";
+
+/**
+ * Disable canvas context in test
+ */
+HTMLCanvasElement.prototype.getContext = function () {
+  // return whatever getContext has to return
+  return null;
+};
