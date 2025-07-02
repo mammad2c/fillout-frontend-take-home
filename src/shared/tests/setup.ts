986a0a9a8f5673599cfdd229c61fc582ee1ce928
Mohammad Toosi <m_dd@rogers.com>
2025-07-02 22:01:17 +0200

    docs: add comment about canvas mock

--- src/shared/tests/setup.ts
+++ src/shared/tests/setup.ts
@@ -1,7 +1,9 @@
 import "@testing-library/jest-dom";
 
 /**
- * Disable canvas context in test
+ * Disable canvas context in test:
+ * https://stackoverflow.com/a/48837087/5158372
+ * TODO: if you have a better way to do this, please let me know
  */
 HTMLCanvasElement.prototype.getContext = function () {
   // return whatever getContext has to return
